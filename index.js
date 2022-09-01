3cefc05ad5d6274e68507881372c3236edaa1dae
HazardCodeBolt <76S2118@ibrict.edu.om>
2022-09-01 18:30:16 +0400

    made the image inner accordion working with add and delete feature functional

--- index.js
+++ index.js
@@ -93,3 +93,42 @@ function remove(element) {
 }
 // end of adding categories to dropdown and to categories holder
 // --------------------------------------------------------------------------------
+// strat of adding accordion image item and deleting it
+function isImage(url) {
+  return /(jpg|jpeg|png|webp|avif|gif|svg)/.test(url);
+}
+
+var innerAccordion = document.getElementById("innerAccordion");
+var imageUrlField = document.getElementById("imageUrlField");
+var addImageButton = document.getElementById("addImage");
+
+addImageButton.addEventListener("click", function () {
+  let url = imageUrlField.value;
+  if (url !== "" && isImage(url)) {
+    let item = document.createElement("div");
+    let index = innerAccordion.childElementCount + 3;
+    console.log(index);
+    item.classList.add("accordion-item");
+    item.innerHTML = `
+    <h2 class="accordion-header" id="heading${index}">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="false" aria-controls="collapse${index}">
+    Image #${index} : ${url}
+    </button>
+    </h2>
+    <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#innerAccordion">
+    <div class="accordion-body"> 
+    <img src="${url}" alt="" class="addedImage">  
+    <button type="button" onclick="removeItem(this)" class="btn btn-danger d-block mt-3 mb-3" style="margin-left: auto; margin-right: auto;">
+    remove
+    </button>  
+    </div>
+    </div>`;
+    innerAccordion.appendChild(item);
+  }
+});
+
+function removeItem(element) {
+  element.parentElement.parentElement.parentElement.remove();
+}
+// strat of adding accordion image item and deleting it
+// --------------------------------------------------------------------------------
